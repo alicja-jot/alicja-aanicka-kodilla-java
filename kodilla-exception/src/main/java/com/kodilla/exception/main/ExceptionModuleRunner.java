43db36d7a5a834138b956b0a5f2203de554b9101
Alicja-Jot <miedzinska93@gmail.com>
2020-01-03 18:22:31 +0100

    obsluga wyjatku

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,13 +1,27 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ExceptionModuleRunner {
     public static void main (String [] args){
 
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
+        try {
+            fileReaderWithoutHandling.readFile();
+        } catch (IOException e) {
+        }
 
+        FileReader fileReader = new FileReader();
+        try{
+            fileReader.readFile();
+        } catch (FileReaderException e){
+            System.out.println("Problem while reading a file!");
+        }
 
 
     }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,20 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(2.5, 5.5);
+        } catch (Exception e){
+            System.out.println("Something went wrong!");
+        } finally {
+            System.out.println("Im always here!");
+        }
+
+
+
+    }
+
+}
