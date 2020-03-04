3f415d020925dbcae73fe7d5a13e476fc5a15d3d
Alicja-Jot <miedzinska93@gmail.com>
2020-03-04 22:10:12 +0100

    Tworzymy encję list zadań

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,36 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class TaskListDaoTestSuite {
+    @Autowired
+
+    private TaskListDao taskListDao;
+
+    @Test
+    public void testFindByListName(){
+        // Given
+        TaskList taskList = new TaskList("Done", "description");
+        taskListDao.save(taskList);
+        String name = taskList.getListName();
+        // When
+        List<TaskList> readTaskList = taskListDao.findByListName(name);
+        // Then
+        Assert.assertEquals(1, readTaskList.size());
+        // CleanUp
+        int id = readTaskList.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+
+}
